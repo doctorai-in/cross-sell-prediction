44ba689e1dc9f1df6d7a5c787361d35fb125b729
Yogender Singh <syogender799@gmail.com>
2020-11-24 17:13:30 +0530

    correlation method added

--- data_preprocessing_v2.py
+++ data_preprocessing_v2.py
@@ -1,7 +1,7 @@
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
-
+from IPython.display import display
 class Data_Preprocessing:
    
     def __init__(self):
@@ -28,7 +28,8 @@ class Data_Preprocessing:
         df = categorical_df.copy()
         columns  = list(df.columns)
         columns.remove(key)
-        print("** " + data_type + " **")
+        print(" ")
+        print("## :: " + data_type + " :: ##")
         for col in columns:
             '''if col == custom_encode_col :
                 if data_type=='Train':
@@ -64,6 +65,25 @@ class Data_Preprocessing:
         
         return merged_df
     
+    def corr_method_pandas(self, categorical, numerical):
+        '''
+        Find correlation using Pandas :
+        '''
+        corre = pd.concat([categorical,  numerical], axis=1, keys=['categorical', 'numerical']).corr().loc['numerical', 'categorical']
+        display(corre)
+        
+    
+    def corr_method_numpy(self, df1, df2):
+        '''
+        Find correlation using Numpy :
+        '''
+        n = len(df1)
+        v1, v2 = df1.values, df2.values
+        sums = np.multiply.outer(v2.sum(0), v1.sum(0))
+        stds = np.multiply.outer(v2.std(0), v1.std(0))
+        return pd.DataFrame((v2.T.dot(v1) - sums / n) / stds / n,
+                            df2.columns, df1.columns)    
+    
     def data_processing_pipeline(self, df, drop_columns , label_column, key, data_type = 'Train', custom_encode_col=None):
         '''
         df : Dataframe
@@ -75,22 +95,40 @@ class Data_Preprocessing:
         key : key is column name through which we merge dataframe
         custom_encode_col : column name use for to encode with custom define encode values.
         
-        RETURN 1. TRAIN ---> X,Y
+        RETURN 1. TRAIN ---> X,Y, final_df
                2. TEST  ---> X
         '''
         if data_type == 'Train':
             categorical_df, numerical_df = self._dataSplit_type(df, key)
             categorical_df               = self.label_encoding(categorical_df, key, data_type, custom_encode_col)
+            
+            print("")
+            print(":: Pandas correlation : Categorical VS Numerical :: ")
+            self.corr_method_pandas(categorical_df, numerical_df)
+            
+            print("")
+            print(":: Pandas correlation : Categorical VS Categorical :: ")
+            self.corr_method_pandas(categorical_df, categorical_df)
+            
             final_df                     = self.merge_df(categorical_df, numerical_df, key)
-            X = self.drop_columns(final_df, drop_columns)
-            Y = final_df[label_column]
+            X                            = self.drop_columns(final_df, drop_columns)
+            Y                            = final_df[label_column]
             return X, Y
         
         if data_type == 'Test':
             categorical_df, numerical_df = self._dataSplit_type(df, key)
             categorical_df               = self.label_encoding(categorical_df, key, data_type, custom_encode_col)
+            
+            print("")
+            print(":: Pandas correlation : Categorical VS Numerical :: ")
+            self.corr_method_pandas(categorical_df, numerical_df)
+            
+            print("")
+            print(":: Pandas correlation : Categorical VS Categorical :: ")
+            self.corr_method_pandas(categorical_df, categorical_df)
+            
             final_df                     = self.merge_df(categorical_df, numerical_df, key)
-            X = self.drop_columns(final_df, drop_columns)
+            X                            = self.drop_columns(final_df, drop_columns)
             
             return X
             
